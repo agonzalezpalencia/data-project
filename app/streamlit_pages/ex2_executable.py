8ab3dc86af489ebd37d089dbfd6d4b815f1ac362
agonzalezpalencia <agonzalezpalencia@stemdo.io>
2026-05-24 20:21:28 +0200

    feat: Add questions_answer() method to ex2_executable in order to answer 360learning second exercise questions

--- app/streamlit_pages/ex2_executable.py
+++ app/streamlit_pages/ex2_executable.py
@@ -31,6 +31,29 @@ def exercise2_metric_1():
     st.scatter_chart(ex2_df_customers_orders, x="Ciudad", x_label="Ciudades", y="Ratio de pedidos por cliente")
 
 
+def questions_answer():
+
+    # Pregunta Nº 1 -  ¿Qué información o patrones se pueden identificar a partir de estos datos?
+    st.title('Respuesta a las preguntas formuladas\n'
+    '- ¿Qué información o patrones se pueden identificar a partir de estos datos?')
+
+    # Respuesta a Pregunta Nº 1
+    st.markdown('> Podemos ver como Sao Paulo es el líder indiscutible del total de pedidos, ya que concentra un 15,63%, seguido de Rio de Janeiro con un 6,92%.' \
+    'En los países del sureste de Brasil, podemos ver como hay una concentración geográfica a tener en cuenta, debido a que la gran mayoría de los pedidos se agrupan sobre esa aparte, ' \
+    'concretamente en los estados de SP,RJ,MG y DF, Los datos también nos dicen como el ratio de pedidos por cliente se situa entre 2 y 3 en las ciudades que menor población tienen' \
+    ', mientras que en las grandes ciudades este ratio se mantiene entre 1 y 1.5, indicando que los clientes en las grandes ciudades solo suelen hacer 1 pedido y en pocas ocasiones 2.')
+
+     # Pregunta Nº 2 -  ¿Qué acciones, como analista de datos, crees que debería tomar la empresa para mejorar sus ventas?
+    st.markdown('- ¿Qué acciones, como analista de datos, crees que debería tomar la empresa para mejorar sus ventas?')
+
+    # Respuesta a Pregunta Nº 2
+    st.markdown('> Para mejorar sus ventas la empresa puede tomar varias acciones, las más importantes y que más impacto pueden llegar a tener son: Reforzar la logística y '
+    'el marketing en las ciudades del sureste puesto a que tienen un volumen de clientela fija y consolidado, manteniendo esos mercados a flote y poder crecer en consecuencia, otra'
+    'de las cosas que podría mejorar las ventas es, aumentar el ratio de los pedidos de los clientes utilizando programas de fidelización de estos mismos en base a puntos '
+    'que puedan usar en los pedidos, o descuentos en los mismos, por último, las ciudades que tienen un ratio alto de pedidos pero población baja podrían ser'
+    'candidatos a programas de captación de clientes, sumando a los clientes recurrentes que ya tienen los captados con estas campañas')
+
 # Protegemos la ejecución para que se ejecute únicamente cuando llamamos al archivo
 if __name__ == "__main__":
     exercise2_metric_1()
+    questions_answer()
